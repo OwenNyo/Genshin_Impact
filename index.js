b46128ba7818d756289eefeeb79010b4f6f2d0b1
OwenNyo <owennyowy@gmail.com>
2021-12-18 10:35:55 +0800

    Updated Image Carousel / Images
    - Added Image Carousel
    - Added JS to run image carousel
    - Added CSS

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let searchQuery = "";
 let results = document.querySelector(".result");
 let search = document.getElementById("search__input");
 const genshinListEl = document.querySelector(".genshins");
+var slideIndex = 0;
+showSlides();
 
 function toggleContrast() {
   contrastToggle = !contrastToggle;
@@ -67,5 +69,22 @@ function skeleton() {
                       <div class="skeleton-type skeleton"></div>
                   </div>`
   genshinListEl.innerHTML = skeletonHTML.repeat(1);
+}
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
 }
 
